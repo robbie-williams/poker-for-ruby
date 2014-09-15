4339fb8fe6c91a3fb52309be2607349bb30252d9
Robbie <c3062701@gmail.com>
2014-09-15 08:46:55 +0000

    Actual gameplay code

--- poker_test.rb
+++ poker_test.rb
@@ -17,12 +17,25 @@ load 'deck-of-cards.rb'
         def initialize num_of_players, num_of_draws
             @@deck.shuffle
             @num_of_draws = num_of_draws
+            @draws = 0
             @players = Array.new(num_of_players){|p| p = Player.new}
             @players.each do |p|
                 p.cards = Array.new(5){|c| c = get_card }
             end
         end
-
+        
+        def get_player_hand player
+                index  = 1
+                player.cards.each do |c|
+                    puts "found a nil" if c.nil?
+                end
+                hand = RRP::PokerHand.new player.cards
+                puts 'Player #' + player.id.to_s + '( ' + hand.rank + '):'
+                player.cards.each do |c| 
+                    puts index.to_s + ") " + c.to_s
+                    index = index + 1
+                end
+        end
 
         def get_player_hands
             @players.each do |p|
@@ -37,7 +50,6 @@ load 'deck-of-cards.rb'
                     index = index + 1
                 end
             end
-          RRP::Card.new  
         end
 
         def get_winner
@@ -54,12 +66,48 @@ load 'deck-of-cards.rb'
             return winner
         end
 
+        def draw player
+            hand_changed = false
+            while( !hand_changed )
+                puts"Player " + player.id.to_s + ", please input cards to discard:"
+                discards = $stdin.gets
+                discards = discards.split(' ')
+                return if discards.length == 0
+                discards.each do |d|
+                    player.cards.each do |c|
+                        if c.to_s.eql? d.to_s.strip
+                            player.cards.delete c
+                            hand_changed = true
+                            break
+                        end
+                    end
+                end
+                puts "Invalid cards selected, either select no cards or choose those from your hand" if !hand_changed
+            end
+            while player.cards.length < 5
+                player.cards << @@deck.draw
+            end
+        end
+
         def get_card
             card = @@deck.draw
             if card.nil?
                 raise 'Ran out of cards in the deck'
             end
             return card
+        end
+
+        def is_finished?
+            @num_of_draws <=  @draws
+        end
+
+        def next_state
+           puts "Performing draw " + (@draws + 1).to_s + " of " + @num_of_draws.to_s
+           @players.each do |p|
+               get_player_hand p
+               draw p
+           end
+           @draws = @draws + 1
         end
     end
 end
@@ -87,6 +135,7 @@ if people.nil?
     invalid_args = true
 else
     people = people.to_i
+    puts "go play with yourself..." if people == 1
 end
 
 draws = ARGV[1]
@@ -114,9 +163,11 @@ if invalid_args
 end
 
 game = Poker_Test::Game.new people, 2
+while !game.is_finished?
+    game.next_state
+end
+
 game.get_player_hands
-
-
 
 player = game.get_winner
 winning_hand = RRP::PokerHand.new(player.cards)
